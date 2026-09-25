e1d24255002954f1920612d76286f75bdb86bbc9
haruisi <hi.haru08@icloud.com>
2026-09-26 01:19:33 +0900

    Initialize radical Reco surface

--- reco/radical-ui.js
+++ reco/radical-ui.js
@@ -1,3 +1,9 @@
 (() => {
-  console.log('Reco radical UI loaded');
+  const home = document.querySelector('#view-home');
+  if (!home) return;
+  const surface = document.createElement('div');
+  surface.id = 'radicalSurface';
+  surface.className = 'radical-surface';
+  surface.innerHTML = '<div class="radical-empty">Reco</div>';
+  home.prepend(surface);
 })();
